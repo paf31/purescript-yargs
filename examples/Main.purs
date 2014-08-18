ee6878c9f8ab49f3b09ee3486f52c3f1031606ce
Phil Freeman <paf31@cantab.net>
2014-08-18 14:14:00 -0700

    Support more options, use YargsSetup type

--- examples/Main.purs
+++ examples/Main.purs
@@ -7,30 +7,38 @@ import Node.Yargs
 
 import Debug.Trace
 
-import Control.Bind
-
-data Args = Args { x :: String, y :: String }
+data Args = Args 
+  { x :: String
+  , y :: String 
+  , reverse :: Boolean
+  }
 
 instance readForeignArgs :: ReadForeign Args where
   read = do
     x <- prop "x"
     y <- prop "y"
-    return $ Args { x: x, y: y }
+    reverse <- prop "reverse"
+    return $ Args { x: x, y: y, reverse: reverse }
 
 main = do
-  let getArgs = usage "$0 -x Word1 -y Word2" 
-                >=> example "$0 -x Hello -y World" "Say hello!"
-		>=> alias "x" "first"
-                >=> alias "y" "second"
-                >=> demand "x" "First argument is required"
-                >=> demand "y" "Second argument is required"
-                >=> requiresArg "x"
-		>=> requiresArg "y"
- 		>=> describe "x" "The first word"
-                >=> describe "y" "The second word"
-                >=> argv
+  let setup = usage "$0 -x Word1 -y Word2" 
+                <> example "$0 -x Hello -y World" "Say hello!"
+	        <> alias "x" "first"
+                <> alias "y" "second"
+                <> alias "r" "reverse"
+                <> demand "x" "First argument is required"
+                <> demand "y" "Second argument is required"
+                <> string "x"
+                <> string "y"
+		<> boolean "r"
+                <> requiresArg "x"
+		<> requiresArg "y"
+ 		<> describe "x" "The first word"
+                <> describe "y" "The second word"
+		<> describe "r" "Reverse the words"
 
-  args <- yargs >>= getArgs
+  args <- runYargs setup
   case parseForeign read args of
     Left err -> print err
+    Right (Args o) | o.reverse -> trace $ o.y ++ " " ++ o.x
     Right (Args o) -> trace $ o.x ++ " " ++ o.y
--- src/Node/Yargs.purs
+++ src/Node/Yargs.purs
@@ -1,15 +1,26 @@
 module Node.Yargs 
   ( Yargs()
+  , YargsSetup()
   , Console()
   
   , yargs
+  , runYargs
   , argv
+  , setupWith
   , usage
   , example
   , alias
   , demand
   , requiresArg
   , describe
+  , boolean
+  , string
+  , config
+  , wrap
+  , strict
+  , help
+  , version
+  , showHelpOnFail
   ) where
 
 import Data.Foreign
@@ -19,6 +30,8 @@ import Control.Monad.Eff
 
 foreign import data Yargs :: *
 
+foreign import data YargsSetup :: *
+
 foreign import data Console :: !
 
 foreign import yargs
@@ -26,23 +39,56 @@ foreign import yargs
   \  return require('yargs');\
   \}" :: forall eff. Eff (console :: Console | eff) Yargs
 
+instance semigroupYargsSetup :: Semigroup YargsSetup where
+  (<>) = unsafeForeignFunction ["s1", "s2", "y"] "s2(s1(y))"
+
+setupWith :: forall eff. YargsSetup -> Yargs -> Eff (console :: Console | eff) Yargs 
+setupWith = unsafeForeignFunction ["setup", "y", ""] "setup(y)"
+
 argv :: forall eff. Yargs -> Eff (console :: Console | eff) Foreign
 argv = unsafeForeignFunction ["y", ""] "y.argv"
 
-usage :: forall eff. String -> Yargs -> Eff (console :: Console | eff) Yargs
-usage = unsafeForeignFunction ["msg", "y", ""] "y.usage(msg)"
+runYargs :: forall eff. YargsSetup -> Eff (console :: Console | eff) Foreign
+runYargs setup = yargs >>= setupWith setup >>= argv
 
-example :: forall eff. String -> String -> Yargs -> Eff (console :: Console | eff) Yargs
-example = unsafeForeignFunction ["cmd", "desc", "y", ""] "y.example(cmd, desc)"
+usage :: String -> YargsSetup
+usage = unsafeForeignFunction ["msg", "y"] "y.usage(msg)"
 
-alias :: forall eff. String -> String -> Yargs -> Eff (console :: Console | eff) Yargs 
-alias = unsafeForeignFunction ["key", "alias", "y", ""] "y.alias(key, alias)"
+example :: String -> String -> YargsSetup
+example = unsafeForeignFunction ["cmd", "desc", "y"] "y.example(cmd, desc)"
 
-demand :: forall eff. String -> String -> Yargs -> Eff (console :: Console | eff) Yargs
-demand = unsafeForeignFunction ["key", "msg", "y", ""] "y.demand(key, msg)"
+alias :: String -> String -> YargsSetup 
+alias = unsafeForeignFunction ["key", "alias", "y"] "y.alias(key, alias)"
 
-requiresArg :: forall eff. String -> Yargs -> Eff (console :: Console | eff) Yargs
-requiresArg = unsafeForeignFunction ["key", "y", ""] "y.requiresArg(key)"
+demand :: String -> String -> YargsSetup
+demand = unsafeForeignFunction ["key", "msg", "y"] "y.demand(key, msg)"
 
-describe :: forall eff. String -> String -> Yargs -> Eff (console :: Console | eff) Yargs
-describe = unsafeForeignFunction ["key", "desc", "y", ""] "y.describe(key, desc)"
+requiresArg :: String -> YargsSetup
+requiresArg = unsafeForeignFunction ["key", "y"] "y.requiresArg(key)"
+
+describe :: String -> String -> YargsSetup
+describe = unsafeForeignFunction ["key", "desc", "y"] "y.describe(key, desc)"
+
+boolean :: String -> YargsSetup
+boolean = unsafeForeignFunction ["key", "y"] "y.boolean(key)"
+
+string :: String -> YargsSetup
+string = unsafeForeignFunction ["key", "y"] "y.string(key)"
+
+config :: String -> YargsSetup
+config = unsafeForeignFunction ["key", "y"] "y.config(key)"
+
+wrap :: Number -> YargsSetup
+wrap = unsafeForeignFunction ["cols", "y"] "y.wrap(cols)"
+
+strict :: YargsSetup
+strict = unsafeForeignFunction ["y"] "y.strict()"
+
+help :: String -> String -> YargsSetup
+help = unsafeForeignFunction ["key", "desc"] "y.help(key, desc)"
+
+version :: String -> String -> String -> YargsSetup
+version = unsafeForeignFunction ["version", "key", "desc", "y"] "y.version(version, key, desc)"
+
+showHelpOnFail :: Boolean -> String -> YargsSetup
+showHelpOnFail = unsafeForeignFunction ["enable", "msg", "y"] "y.showHelpOnFail(enable, msg)"
