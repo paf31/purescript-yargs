418d685ba1303a3a674a0569a1bbf200e3add6ca
Phil Freeman <paf31@cantab.net>
2014-08-19 19:08:59 -0700

    Update for new purescript-foreign

--- src/Node/Yargs/Applicative.purs
+++ src/Node/Yargs/Applicative.purs
@@ -6,6 +6,7 @@ module Node.Yargs.Applicative
   ) where
 
 import Data.Foreign
+import Data.Foreign.Class
 import Data.Monoid
 import Data.Either
 
@@ -17,25 +18,25 @@ import Control.Monad.Eff.Unsafe
 import Control.Monad.Eff.Exception
 
 newtype Y a = Y { setup :: YargsSetup
-                , read  :: ForeignParser a
+                , read  :: Foreign -> F a
                 }
 
 instance functorY :: Functor Y where
-  (<$>) f (Y o) = Y { setup: o.setup, read: f <$> o.read }
+  (<$>) f (Y o) = Y { setup: o.setup, read: \value -> f <$> o.read value }
 
 instance applyT :: Apply Y where
   (<*>) (Y o1) (Y o2) = Y { setup: o1.setup <> o2.setup
-                          , read: o1.read <*> o2.read
+                          , read: \value -> o1.read value <*> o2.read value
                           }
 
 instance applicativeY :: Applicative Y where
-  pure a = Y { setup: mempty, read: pure a }
+  pure a = Y { setup: mempty, read: \_ -> pure a }
 
 runY :: forall a eff. YargsSetup -> Y (Eff eff a) -> Eff (err :: Exception, console :: Console | eff) a
 runY setup (Y y) = do
   value <- runYargs (setup <> y.setup)
-  case parseForeign y.read value of
-    Left err -> throwException (error err)
+  case y.read value of
+    Left err -> throwException (error (show err))
     Right action -> unsafeInterleaveEff action
 
 class Arg a where
@@ -43,30 +44,30 @@ class Arg a where
 
 instance argString :: Arg String where
   arg key = Y { setup: string key 
-              , read: prop key
+              , read: readProp key
               }
 
 instance argBoolean :: Arg Boolean where
   arg key = Y { setup: boolean key 
-              , read: prop key
+              , read: readProp key
               }
 	      
 instance argNumber :: Arg Number where
   arg key = Y { setup: mempty 
-              , read: prop key
+              , read: readProp key
               }
 
 instance argStrings :: Arg [String] where
   arg key = Y { setup: string key 
-              , read: prop key
+              , read: readProp key
               }
 
 instance argBooleans :: Arg [Boolean] where
   arg key = Y { setup: boolean key 
-              , read: prop key
+              , read: readProp key
               }
 	      
 instance argNumbers :: Arg [Number] where
   arg key = Y { setup: mempty 
-              , read: prop key
+              , read: readProp key
               }
