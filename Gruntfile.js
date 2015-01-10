7e011d1c2d492fb6a0ee6b2975df3e401b864195
Gary Burgess <gary.burgess@gmail.com>
2015-01-10 15:32:09 +0000

    Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,15 +2,15 @@ module.exports = function(grunt) {
 
   "use strict";
 
-  grunt.initConfig({ 
-  
+  grunt.initConfig({
+
     libFiles: [
       "src/**/*.purs",
       "bower_components/purescript-*/src/**/*.purs"
     ],
-    
+
     clean: ["tmp", "output"],
-  
+
     pscMake: {
       lib: {
         src: ["<%=libFiles%>"]
@@ -22,7 +22,7 @@ module.exports = function(grunt) {
 
     dotPsci: ["<%=libFiles%>"],
 
-    docgen: {
+    pscDocs: {
       readme: {
         src: "src/**/*.purs",
         dest: "docs/README.md"
@@ -45,15 +45,15 @@ module.exports = function(grunt) {
       examples: {
 	src: "tmp/index.js"
       }
-    }      
+    }
   });
 
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-clean");
   grunt.loadNpmTasks("grunt-execute")
   grunt.loadNpmTasks("grunt-purescript");
- 
+
   grunt.registerTask("test", ["pscMake:examples", "copy", "execute:examples"]);
-  grunt.registerTask("make", ["pscMake:lib", "dotPsci", "docgen:readme"]);
+  grunt.registerTask("make", ["pscMake:lib", "dotPsci", "pscDocs:readme"]);
   grunt.registerTask("default", ["clean", "make", "test"]);
 };
