a517d190f15e7817a3144a982663281f125d28e8
Nick Saunders <nick@saunde.rs>
2018-06-19 13:40:07 -0700

    Updates for PureScript 0.12 (#17)
    * Updates for PureScript 0.12
    
    * Update pulp/purescript versions in package.json; minor formatting

--- src/Node/Yargs.purs
+++ src/Node/Yargs.purs
@@ -11,9 +11,8 @@ module Node.Yargs
   ) where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Data.Foreign (Foreign)
+import Effect (Effect)
+import Foreign (Foreign)
 import Node.Yargs.Setup (YargsSetup)
 
 -- | The type of the `yargs` module, i.e. the result of
@@ -21,22 +20,21 @@ import Node.Yargs.Setup (YargsSetup)
 foreign import data Yargs :: Type
 
 -- | Get a reference to the `yargs` module.
-foreign import yargs :: forall eff. Eff (console :: CONSOLE | eff) Yargs
+foreign import yargs :: Effect Yargs
 
 -- | Setup the `yargs` module.
 -- |
 -- | See the `Node.Yargs.Setup` module, which contains functions for creating
 -- | values of type `YargsSetup`.
 foreign import setupWith
-  :: forall eff
-   . YargsSetup
+  :: YargsSetup
   -> Yargs
-  -> Eff (console :: CONSOLE | eff) Yargs
+  -> Effect Yargs
 
 -- | Get the raw command line arguments object, as a foreign value.
-foreign import argv :: forall eff. Yargs -> Eff (console :: CONSOLE | eff) Foreign
+foreign import argv :: Yargs -> Effect Foreign
 
 -- | Setup the `yargs` module and get the resulting parsed command line
 -- | arguments object.
-runYargs :: forall eff. YargsSetup -> Eff (console :: CONSOLE | eff) Foreign
+runYargs :: YargsSetup -> Effect Foreign
 runYargs setup = yargs >>= setupWith setup >>= argv
